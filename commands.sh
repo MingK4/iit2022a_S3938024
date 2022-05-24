4ff2f93ac25efeebe36c582f5f3543e96b7bf672
Ming <102708759+MingK4@users.noreply.github.com>
2022-05-24 09:48:48 +0700

    redo

--- commands.sh
+++ commands.sh
@@ -2,14 +2,14 @@ touch iit_answer.txt
 
 echo "All answers are very easy to find" > iit_answer.txt
 
+git push -u https://github.com/MingK4/iit2022a_S3938024
+
 git branch branch_easy
 
 
 git checkout branch_easy
 
-
-echo "All answer are easy to find" > iit_answer.txt
-
+echo "All answer are easy to find" >> iit_answer.txt
 
 git add iit_answer.txt
 
@@ -17,13 +17,15 @@ git add iit_answer.txt
 git commit -m "updated iit_answer"
 
 
+git push -u https://github.com/MingK4/iit2022a_S3938024
+
 git branch branch_noteasy
 
 
 git checkout branch_noteasy
 
 
-echo "All answers are not easy to find" > iit_answer.txt
+echo "All answers are not easy to find" >> iit_answer.txt
 
 
 git add iit_answer.txt
@@ -32,16 +34,20 @@ git add iit_answer.txt
 git commit -m "updated again iit_answer"
 
 
+git push -u https://github.com/MingK4/iit2022a_S3938024
+
 git branch hard
 
 
 git checkout hard
 
 
-echo "All answers are hard to find" > iit_answer.txt
+echo "All answers are hard to find" >> iit_answer.txt
 
 
 git add iit_answer.txt
 
 
 git commit -m "updated hard iit_answer"
+
+git push -u https://github.com/MingK4/iit2022a_S3938024
