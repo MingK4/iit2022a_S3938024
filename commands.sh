be1a103fd5fe19f33b76c6a7ffe14b53c6a410c1
Ming <102708759+MingK4@users.noreply.github.com>
2022-05-24 10:25:24 +0700

    new cmd

--- commands.sh
+++ commands.sh
@@ -2,50 +2,29 @@ touch iit_answer.txt
 
 echo "All answers are very easy to find" > iit_answer.txt
 
-<<<<<<< HEAD
 git add iit_answer.txt
 
-git commit -m "add txt"
+git commit -m "First"
 
-=======
->>>>>>> a5a5590de66892fd2dd71cd4bf73606263ca96dc
 git push -u https://github.com/MingK4/iit2022a_S3938024
 
 git branch branch_easy
 
-<<<<<<< HEAD
-=======
 
->>>>>>> a5a5590de66892fd2dd71cd4bf73606263ca96dc
 git checkout branch_easy
 
 echo "All answer are easy to find" >> iit_answer.txt
 
 git add iit_answer.txt
 
-<<<<<<< HEAD
-git commit -m "updated iit_answer"
-
-=======
 
 git commit -m "updated iit_answer"
 
 
->>>>>>> a5a5590de66892fd2dd71cd4bf73606263ca96dc
 git push -u https://github.com/MingK4/iit2022a_S3938024
 
 git branch branch_noteasy
 
-<<<<<<< HEAD
-git checkout branch_noteasy
-
-echo "All answers are not easy to find" >> iit_answer.txt
-
-git add iit_answer.txt
-
-git commit -m "updated again iit_answer"
-
-=======
 
 git checkout branch_noteasy
 
@@ -59,19 +38,10 @@ git add iit_answer.txt
 git commit -m "updated again iit_answer"
 
 
->>>>>>> a5a5590de66892fd2dd71cd4bf73606263ca96dc
 git push -u https://github.com/MingK4/iit2022a_S3938024
 
 git branch hard
 
-<<<<<<< HEAD
-git checkout hard
-
-echo "All answers are hard to find" >> iit_answer.txt
-
-git add iit_answer.txt
-
-=======
 
 git checkout hard
 
@@ -82,7 +52,6 @@ echo "All answers are hard to find" >> iit_answer.txt
 git add iit_answer.txt
 
 
->>>>>>> a5a5590de66892fd2dd71cd4bf73606263ca96dc
 git commit -m "updated hard iit_answer"
 
 git push -u https://github.com/MingK4/iit2022a_S3938024
